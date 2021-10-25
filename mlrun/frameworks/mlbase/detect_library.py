6db8661fd9f2be08cd7f5d48e03f241f9fb8835a
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-10-25 16:20:53 +0300

    Update detect_library.py

--- mlrun/frameworks/mlbase/detect_library.py
+++ mlrun/frameworks/mlbase/detect_library.py
@@ -1,27 +1,20 @@
 def detect_model_library(model) -> bool:
     """
-    Identifies which of the main three Machine Learning library a model is using
+    Identifies which of the main three Machine Learning library a model is using.
 
     :param model:           fitted or unfitted model from xgboost, sklearn or lgbm.
 
-    :return is_sklearn:     a boolean value equivalent to sklearn.
-    :return is_xgb:         a boolean value equivalent to sklearn
-    :return is_lgbm:        a boolean value equivalent to sklearn
+    :return is_sklearn:     a boolean equivalent to sklearn.
+    :return is_xgb:         a boolean equivalent to xgboost.
+    :return is_lgbm:        a boolean equivalent to lightgbm.
     """
-
-    is_sklearn = False
-    is_xgb = False
-    is_lgbm = False
-
+    
+    # Parse library from model class
     model_library = str(model.__class__.__module__).split('.')[0]
-
-    if model_library == 'sklearn':
-        is_sklearn = True
-
-    elif model_library == 'xgboost':
-        is_xgb = True
-
-    elif model_library == 'lightgbm':
-        is_lgbm = True
+    
+    # Turn the according boolean flags
+    is_sklearn = True if model_library == 'sklearn' else False
+    is_xgb = True if model_library == 'xgboost' else False
+    is_lgbm = True if model_library == 'lightgbm' else False
 
     return is_sklearn, is_xgb, is_lgbm
