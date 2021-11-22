400c38b90cf39d95a19260caa54f918b2f5d2fc8
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-11-22 12:49:08 +0200

    Add files via upload

---
+++ mlrun/frameworks/_common/library.py
@@ -0,0 +1,203 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from plan import ProductionStages
+from abc import ABC, abstractmethod
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+from typing import List
+from mlrun.artifacts import PlotArtifact
+from plan import ArtifactPlan
+
+
+class ArtifactLibrary(ABC):
+    """
+    An abstract class for an artifacts library. Each framework should have an artifacts library for knowing what
+    artifacts can be produced and their configurations. The default method must be implemented for when the user do not
+    pass any plans.
+    """
+
+    @classmethod
+    @abstractmethod
+    def default(cls) -> List[ArtifactPlan]:
+        """
+        Get the default artifacts plans list of this framework's library.
+
+        :return: The default artifacts plans list.
+        """
+        pass
+
+    def df_blob(df):
+        """
+        """
+        return bytes(df.to_csv(index=False), encoding="utf-8")
+
+    def check_multiclass(self, ytest):
+        """
+        """
+        if isinstance(ytest, (pd.DataFrame, pd.Series)):
+            ytest = ytest.to_numpy()
+        if isinstance(ytest, np.ndarray):
+            self._unique_labels = np.unique(ytest)
+        elif isinstance(ytest, list):
+            self._unique_labels = set(ytest)
+        return True if len(self._unique_labels) > 2 else False
+
+    class ConfusionMatrix(ArtifactPlan):
+        """
+        """
+
+        def __init__(
+                self,
+                labels=None,
+                sample_weight=None,
+                normalize=None,
+                display_labels=None,
+                include_values=True,
+                xticks_rotation="horizontal",
+                values_format=None,
+                cmap="Blues",
+                ax=None,
+                **kwargs):
+            
+            # confusion_matrix() parameters
+            self._labels = labels
+            self._sample_weight = sample_weight
+            self._normalize = normalize
+
+            # plot_confusion_matrix() parameters
+            self._cmap = cmap
+            self._values_format = values_format
+            self._display_labels = display_labels
+            self._include_values = include_values
+            self._xticks_rotation = xticks_rotation
+            self._ax = ax
+
+            # other
+            self._kwargs = kwargs
+            self._extra_data = {}
+
+        def validate(self, *args, **kwargs):
+            pass
+
+        def is_ready(self, stage: ProductionStages) -> bool:
+            return stage == ProductionStages.POST_FIT
+
+        def produce(self, model, context, apply_args, plots_artifact_path="", **kwargs):
+            # generate confusion matrix
+            cm = confusion_matrix(
+                apply_args['y_test'],
+                apply_args['y_pred'],
+                labels=self._labels,
+                sample_weight=self._sample_weight,
+                normalize=self._normalize,
+            )
+
+            # Create a dataframe from cmatrix
+            df = pd.DataFrame(data=cm)
+
+            # Add the dataframe to extra_data
+            self._extra_data["confusion_matrix_table.csv"] = ArtifactLibrary.df_blob(df)
+
+            cmd = metrics.plot_confusion_matrix(
+                model,
+                apply_args['X_test'],
+                apply_args['y_test'],
+                labels=self._labels,
+                sample_weight=self._sample_weight,
+                normalize=self._normalize,
+                display_labels=self._display_labels,
+                include_values=self._include_values,
+                xticks_rotation=self._xticks_rotation,
+                values_format=self._values_format,
+                cmap=self._cmap,
+                ax=self._ax,
+            )
+
+            self._extra_data["confusion matrix"] = context.log_artifact(
+                PlotArtifact(
+                    "confusion-matrix",
+                    body=cmd.figure_,
+                    title="Confusion Matrix - Normalized Plot",
+                ),
+                artifact_path=plots_artifact_path or context.artifact_subpath("plots"),
+                db_key=False,
+            )
+
+            return self._extra_data
+
+        
+    class FeatureImportance(ArtifactPlan):
+        """
+        """
+
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+            self._extra_data = {}
+
+        def validate(self, *args, **kwargs):
+            pass
+
+        def is_ready(self, stage: ProductionStages) -> bool:
+            return stage == ProductionStages.POST_FIT
+
+        def produce(self, model, context, apply_args, plots_artifact_path="", **kwargs):
+
+            """Display estimated feature importances
+            Only works for models with attribute 'feature_importances_`
+            Feature importances are provided by the fitted attribute feature_importances_ and they are computed 
+            as the mean and standard deviation of accumulation of the impurity decrease within each tree.
+
+            :param model:       fitted model
+            :param header:      feature labels
+            """
+
+            # Tree-based feature importance
+            if hasattr(model, "feature_importances_"):
+                importance_score = zip(model.feature_importances_, apply_args['X_train'].columns)
+            
+            # Coefficient-based importance
+            elif hasattr(model, "coef_"):
+                importance_score = zip(model.coef_, apply_args['X_train'].columns)
+        
+            feature_imp = pd.DataFrame(sorted(importance_score), columns=["Importance score", "feature"]).sort_values(
+                by="Importance score", ascending=False
+            )
+
+            plt.figure()
+            sns.barplot(x="Importance score", y="feature", data=feature_imp)
+            plt.title("features")
+            plt.tight_layout()
+
+            self._extra_data["feature importance"] = context.log_artifact(
+                PlotArtifact(
+                    "feature-importances", body=plt.gcf(), title="Feature Importances"
+                ),
+                artifact_path=plots_artifact_path or context.artifact_subpath("plots"),
+                db_key=False, )
+
+            return self._extra_data, feature_imp
+
+
+class SkLearnArtifactLibrary(ArtifactLibrary):
+    @classmethod
+    def default(cls) -> List[ArtifactPlan]:
+        return [ArtifactLibrary.ConfusionMatrix(cmap='Blues')]
+
+    @staticmethod
+    def confusion_matrix(a: int = 9, b: float = 9.9, c: str = "9"):
+        return ConfusionMatrixPlan(a=a, b=b, c=c)
+
+    
+class XgbArtifactLibrary(ArtifactLibrary):
+    @classmethod
+    def default(cls) -> List[ArtifactPlan]:
+        return [ArtifactLibrary.ConfusionMatrix(cmap='Blues')]
+    
+    
+class LgbmArtifactLibrary(ArtifactLibrary):
+    @classmethod
+    def default(cls) -> List[ArtifactPlan]:
+        return [ArtifactLibrary.ConfusionMatrix(cmap='Blues')]
---
+++ mlrun/frameworks/_common/plans_manager.py
@@ -0,0 +1,25 @@
+from typing import List, Dict
+
+from mlrun.artifacts import Artifact
+from plan import ArtifactPlan, ProductionStages
+
+
+class ArtifactsPlansManager:
+    def __init__(self, plans: List[ArtifactPlan]):
+        self._plans = plans
+        self._artifacts = []  # type: List[Artifact]
+
+    @property
+    def artifacts(self) -> Dict[str, Artifact]:
+        return {artifact.key: artifact for artifact in self._artifacts}
+
+    def validate(self):
+        #call validation of each plan
+        pass
+
+    def generate(self, model, context, apply_args, mystage: ProductionStages, *args, **kwargs):
+        self._artifacts += [
+            plan.produce(model, context, apply_args, **kwargs)
+            for plan in self._plans
+            if plan.is_ready(stage=mystage)
+        ]
