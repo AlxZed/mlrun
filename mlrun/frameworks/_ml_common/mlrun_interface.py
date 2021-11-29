37687138b4cf1c8dee908999289caf24c41accc7
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-11-29 14:31:01 +0200

    Update mlrun_interface.py

--- mlrun/frameworks/_ml_common/mlrun_interface.py
+++ mlrun/frameworks/_ml_common/mlrun_interface.py
@@ -12,6 +12,26 @@ class MLMLRunInterface:
     Wraps the original .fit() method of the passed model enabling auto-logging.
     """
 
+    @staticmethod
+    def merge_dataframes(x: pd.DataFrame, y: pd.DataFrame) -> pd.DataFrame:
+        """
+        Merge two dataframes while making sure their indices are aligned.
+
+        :param x: train data as a pd.DataFrame
+        :param y: label column as a pd.DataFrame
+        :return: merged dataframe of x and y
+        """
+        # Checking if indexes are aligned
+        assert set(x.index.tolist()) == set(
+            y.index.tolist()
+        ), "Pandas Dataframe indexes are not equal.."
+
+        # Merge X and y for logging of the train set
+        full_df = pd.concat([x, y], axis=1)
+        full_df.reset_index(drop=True, inplace=True)
+
+        return full_df
+
     @classmethod
     def add_interface(
         cls,
@@ -25,14 +45,12 @@ class MLMLRunInterface:
         """
         Wrap the given model with MLRun model features, providing it with MLRun model attributes including its
         parameters and methods.
-
         :param model:       The model to wrap.
         :param context:     MLRun context to work with. If no context is given it will be retrieved via
                             'mlrun.get_or_create_ctx(None)'
         :param model_name:  name under whcih the model will be saved within the databse.
         :param data:        Optional: The train_test_split X_train, X_test, y_train, y_test can be passed,
                                       or the test data X_test, y_test can be passed.
-
         :return: The wrapped model.
         """
         model = model_handler.model
@@ -58,24 +76,26 @@ class MLMLRunInterface:
             context.set_label("class", str(model.__class__.__name__))
 
             # Get passed X,y from model.fit(X,y)
-            x, y = args[0], args[1]
-            # np.array -> Dataframe
-            if isinstance(x, np.ndarray):
-                x = pd.DataFrame(x)
-            if isinstance(y, np.ndarray):
-                y = pd.DataFrame(y)
+            x_train, y_train = args[0], args[1]
 
-            # Merge X and y for logging of the train set
-            train_set = pd.concat([x, y], axis=1)
-            train_set.reset_index(drop=True, inplace=True)
+            # np.array -> Dataframe
+            if isinstance(x_train, np.ndarray) and isinstance(y_train, np.ndarray):
+                x_train, y_train = pd.DataFrame(x_train), pd.DataFrame(y_train)
+
+            # Merge X and y for logging of the test set
+            train_set = MLMLRunInterface.merge_dataframes(x_train, y_train)
 
             if data.get("X_test") is not None and data.get("y_test") is not None:
+
                 # Identify splits and build test set
                 x_test, y_test = data["X_test"], data["y_test"]
 
+                # np.array -> Dataframe
+                if isinstance(x_test, np.ndarray) and isinstance(y_test, np.ndarray):
+                    x_test, y_test = pd.DataFrame(x_test), pd.DataFrame(y_test)
+
                 # Merge X and y for logging of the test set
-                test_set = pd.concat([x_test, y_test], axis=1)
-                test_set.reset_index(drop=True, inplace=True)
+                test_set = MLMLRunInterface.merge_dataframes(x_test, y_test)
 
                 # Evaluate model results and get the evaluation metrics
                 eval_metrics = eval_model_v2(context, x_test, y_test, model)
@@ -93,11 +113,11 @@ class MLMLRunInterface:
 
             # Identify label column
             label_column = None  # type: List[str]
-            if isinstance(y, pd.DataFrame):
-                label_column = y.columns.to_list()
-            elif isinstance(y, pd.Series):
-                if y.name is not None:
-                    label_column = [str(y.name)]
+            if isinstance(y_train, pd.DataFrame):
+                label_column = y_train.columns.to_list()
+            elif isinstance(y_train, pd.Series):
+                if y_train.name is not None:
+                    label_column = [str(y_train.name)]
                 else:
                     raise ValueError("No column name for y was specified")
 
