5c3848d501d095fb640fc3777f90e42d0b0f6223
alxzed <XvyRiF.Lg9FKMU6t.hcrV3fdJ>
2021-12-12 18:31:19 +0200

    roc-plotly

--- mlrun/frameworks/_ml_common/library.py
+++ mlrun/frameworks/_ml_common/library.py
@@ -30,53 +30,53 @@ class ArtifactLibrary(ABC):
         pass
 
 
-class ROCCurves(ArtifactPlan):
-    """
-    """
+    class ROCCurves(ArtifactPlan):
+        """
+        """
 
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
-        self._extra_data = {}
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+            self._extra_data = {}
 
-    def validate(self, *args, **kwargs):
-        pass
+        def validate(self, *args, **kwargs):
+            pass
 
-    def is_ready(self, stage: ProductionStages) -> bool:
-        return stage == ProductionStages.POST_FIT
+        def is_ready(self, stage: ProductionStages) -> bool:
+            return stage == ProductionStages.POST_FIT
 
-    def produce(self, model, context, apply_args, plots_artifact_path="", **kwargs):
-        if hasattr(model, "predict_proba"):
-            y_scores = model.predict_proba(apply_args['X_test'])
+        def produce(self, model, context, apply_args, plots_artifact_path="", **kwargs):
+            if hasattr(model, "predict_proba"):
+                y_scores = model.predict_proba(apply_args['X_test'])
 
-        # One hot encode the labels in order to plot them
-        y_onehot = pd.get_dummies(apply_args['y_test'], columns=model.classes_)
+            # One hot encode the labels in order to plot them
+            y_onehot = pd.get_dummies(apply_args['y_test'], columns=model.classes_)
 
-        # Create an empty figure, and iteratively add new lines
-        # every time we compute a new class
-        fig = go.Figure()
-        fig.add_shape(
-            type='line', line=dict(dash='dash'),
-            x0=0, x1=1, y0=0, y1=1
-        )
+            # Create an empty figure, and iteratively add new lines
+            # every time we compute a new class
+            fig = go.Figure()
+            fig.add_shape(
+                type='line', line=dict(dash='dash'),
+                x0=0, x1=1, y0=0, y1=1
+            )
 
-        for i in range(y_scores.shape[1]):
-            y_true = y_onehot.iloc[:, i]
-            y_score = y_scores[:, i]
+            for i in range(y_scores.shape[1]):
+                y_true = y_onehot.iloc[:, i]
+                y_score = y_scores[:, i]
 
-            fpr, tpr, _ = roc_curve(y_true, y_score)
-            auc_score = roc_auc_score(y_true, y_score)
+                fpr, tpr, _ = roc_curve(y_true, y_score)
+                auc_score = roc_auc_score(y_true, y_score)
 
-            name = f"{y_onehot.columns[i]} (AUC={auc_score:.2f})"
-            fig.add_trace(go.Scatter(x=fpr, y=tpr, name=name, mode='lines'))
+                name = f"{y_onehot.columns[i]} (AUC={auc_score:.2f})"
+                fig.add_trace(go.Scatter(x=fpr, y=tpr, name=name, mode='lines'))
 
-        fig.update_layout(
-            xaxis_title='False Positive Rate',
-            yaxis_title='True Positive Rate',
-            yaxis=dict(scaleanchor="x", scaleratio=1),
-            xaxis=dict(constrain='domain'),
-            width=700, height=500
-        )
-        fig.show()
+            fig.update_layout(
+                xaxis_title='False Positive Rate',
+                yaxis_title='True Positive Rate',
+                yaxis=dict(scaleanchor="x", scaleratio=1),
+                xaxis=dict(constrain='domain'),
+                width=700, height=500
+            )
+            fig.show()
 
 
 
