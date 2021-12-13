795dceb956e656a8ad6b5068618b205d5f03bc49
alxzed <XvyRiF.Lg9FKMU6t.hcrV3fdJ>
2021-12-13 18:22:29 +0200

    Learning Curves

--- mlrun/frameworks/_ml_common/library.py
+++ mlrun/frameworks/_ml_common/library.py
@@ -9,6 +9,8 @@ import numpy as np
 from sklearn.metrics import roc_curve, roc_auc_score
 from IPython.core.display import HTML, display
 from mlrun.artifacts import Artifact
+from sklearn.model_selection import learning_curve
+
 
 class ArtifactLibrary(ABC):
     """
@@ -216,6 +218,79 @@ class FeatureImportance(ArtifactPlan):
         )
 
         fig = go.Figure([go.Bar(x=df['feature_importance'], y=df['features'], orientation='h')])
+
+        # Creating an html rendering of the plot
+        plot_as_html = fig.to_html()
+        display(HTML(plot_as_html))
+
+
+class LearningCurves(ArtifactPlan):
+    """
+    SkLearn Learning Curves
+    """
+
+    def __init__(self,
+                 cv=3,
+                 **kwargs):
+
+        # Learning Curves parameters
+        self._cv = cv
+
+        # Other
+        self.kwargs = kwargs
+        self._extra_data = {}
+
+    @abstractmethod
+    def validate(self, *args, **kwargs):
+        """
+        Validate this plan has the required data to produce its artifact.
+
+        :raise ValueError: In case this plan is missing information in order to produce its artifact.
+        """
+        pass
+
+    @abstractmethod
+    def is_ready(self, stage: ProductionStages) -> bool:
+        return stage == ProductionStages.POST_FIT
+
+    @abstractmethod
+    def produce(self, model, apply_args, *args, **kwargs) -> Artifact:
+        """
+        Produce the artifact according to this plan.
+
+        :return: The produced artifact.
+        """
+
+        train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(model, apply_args['X_train'],
+                                                                              apply_args['y_train'].values.ravel(), cv=self._cv,
+                                                                              return_times=True)
+
+        fig = go.Figure(
+            data=[go.Scatter(x=train_sizes.tolist(), y=np.mean(train_scores, axis=1))],
+            layout=dict(title=dict(text="Learning Curves"))
+        )
+
+        # add custom xaxis title
+        fig.add_annotation(dict(font=dict(color="black", size=14),
+                                x=0.5,
+                                y=-0.15,
+                                showarrow=False,
+                                text="Train Size",
+                                xref="paper",
+                                yref="paper"))
+
+        # add custom yaxis title
+        fig.add_annotation(dict(font=dict(color="black", size=14),
+                                x=-0.1,
+                                y=0.5,
+                                showarrow=False,
+                                text="Score",
+                                textangle=-90,
+                                xref="paper",
+                                yref="paper"))
+
+        # adjust margins to make room for yaxis title
+        fig.update_layout(margin=dict(t=100, l=100), width=800, height=500)
 
         # Creating an html rendering of the plot
         plot_as_html = fig.to_html()
