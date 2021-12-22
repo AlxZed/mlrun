5f32911012e0b2226c6cdbe7f01957a1bf4a9f0e
alxzed <XvyRiF.Lg9FKMU6t.hcrV3fdJ>
2021-12-22 17:05:48 +0200

    new skeleton

--- mlrun/frameworks/_ml_common/library.py
+++ mlrun/frameworks/_ml_common/library.py
@@ -1,8 +1,7 @@
-from typing import Dict, Any
+from typing import Any
 from enum import Enum
 
 import inspect
-
 
 from plotly.figure_factory import create_annotated_heatmap
 from typing import Dict
@@ -17,6 +16,7 @@ from sklearn.metrics import roc_curve, roc_auc_score
 from IPython.core.display import HTML, display
 from mlrun.artifacts import Artifact
 from sklearn.model_selection import learning_curve
+from sklearn.calibration import calibration_curve
 
 
 class PlanStages(Enum):
@@ -230,7 +230,7 @@ def validate_numerical(dataset):
 
 class ConfusionMatrixPlan(Plan):
     """
-    Compute confusion matrix to evaluate the accuracy of a classification with Plotly.
+    Compute confusion matrix to plot the accuracy of a classification with Plotly.
     """
 
     _ARTIFACT_NAME = "confusion_matrix"
@@ -238,6 +238,13 @@ class ConfusionMatrixPlan(Plan):
     def __init__(
         self, labels=None, sample_weight=None, normalize=None, y_test=None, y_pred=None
     ):
+        """
+        :param labels: List of labels to index the matrix.
+        :param sample_weight: Sample weights.
+        :param normalize: Normalizes confusion matrix over the true (rows), predicted (columns) conditions or all the population.
+        :param y_test: Ground truth (correct) target values.
+        :param y_pred: Estimated targets as returned by a classifier.
+        """
         validate_numerical(y_test)
         validate_numerical(y_pred)
 
@@ -249,10 +256,20 @@ class ConfusionMatrixPlan(Plan):
         super(ConfusionMatrixPlan, self).__init__(y_test=y_test, y_pred=y_pred)
 
     def is_ready(self, stage: PlanStages) -> bool:
+        """
+        Check whether or not the plan fits the given stage for production.
+        :param stage: The current stage in the function's process.
+        :return: True if the given stage fits the plan and False otherwise.
+        """
         return stage == PlanStages.POST_EVALUATION
 
     def produce(self, y_test, y_pred, **kwargs) -> Dict[str, PlotlyArtifact]:
-
+        """
+        Produce the plotly artifact according to this plan.
+        :param y_pred: Estimated targets as returned by a classifier.
+        :param y_test: Ground truth (correct) target values.
+        :return: The produced artifacts.
+        """
         cm = confusion_matrix(
             y_test,
             y_pred,
@@ -317,6 +334,10 @@ class ConfusionMatrixPlan(Plan):
         return self._artifacts
 
     def display(self):
+        """
+        Display the plan's artifact. This method will be called by the IPython kernel to showcase the plan. If artifacts
+        were produced this is the method to draw and print them.
+        """
         if self._artifacts:
             display(HTML(self._artifacts[self._ARTIFACT_NAME].get_body()))
         else:
@@ -324,16 +345,57 @@ class ConfusionMatrixPlan(Plan):
 
 
 class ROCCurves(Plan):
+    """ """
+
     _ARTIFACT_NAME = "roc_curves"
 
-    def __init__(self, model=None, X_test=None, y_test=None):
+    def __init__(
+        self,
+        model=None,
+        X_test=None,
+        y_test=None,
+        pos_label=None,
+        sample_weight=None,
+        drop_intermediate=True,
+        average="macro",
+        max_fpr=None,
+        multi_class="raise",
+        labels=None,
+    ):
+        """
+
+        :param model:
+        :param X_test:
+        :param y_test: target set.
+        :param pos_label: The label of the positive class. When pos_label=None, if y_true is in {-1, 1} or {0, 1}, pos_label is set to 1, otherwise an error will be raised.
+        :param sample_weight: Sample weights.
+        :param drop_intermediate: Whether to drop some suboptimal thresholds which would not appear on a plotted ROC curve.
+        :param average: If None, the scores for each class are returned. Otherwise, this determines the type of averaging performed on the data
+        :param max_fpr: If not None, the standardized partial AUC [2] over the range [0, max_fpr] is returned.
+        :param multi_class: Only used for multiclass targets. Determines the type of configuration to use.
+        :param labels: Only used for multiclass targets. List of labels that index the classes in y_score
+        """
+
         validate_numerical(X_test)
         validate_numerical(y_test)
+
+        self._pos_label = pos_label
+        self.sample_weight = sample_weight
+        self.drop_intermediate = drop_intermediate
+        self.average = average
+        self.max_fpr = max_fpr
+        self.multi_class = multi_class
+        self.labels = labels
 
         super(ROCCurves, self).__init__(model=model, X_test=X_test, y_test=y_test)
 
     def is_ready(self, stage: PlanStages) -> bool:
-        return stage == PlanStages.POST_EVALUATION
+        """
+        Check whether or not the plan fits the given stage for production.
+        :param stage: The current stage in the function's process.
+        :return: True if the given stage fits the plan and False otherwise.
+        """
+        return stage == PlanStages.POST_FIT
 
     def produce(self, model, X_test, y_test, **kwargs) -> Dict[str, PlotlyArtifact]:
         """
@@ -355,8 +417,23 @@ class ROCCurves(Plan):
                 y_true = y_onehot.iloc[:, i]
                 y_score = y_scores[:, i]
 
-                fpr, tpr, _ = roc_curve(y_true, y_score)
-                auc_score = roc_auc_score(y_true, y_score)
+                fpr, tpr, _ = roc_curve(
+                    y_true,
+                    y_score,
+                    pos_label=self._pos_label,
+                    sample_weight=self._sample_weight,
+                    drop_intermediate=self._drop_intermediate,
+                )
+
+                auc_score = roc_auc_score(
+                    y_true,
+                    y_score,
+                    average=self._average,
+                    sample_weight=self._sample_weight,
+                    max_fpr=self._max_fpr,
+                    multi_class=self._multi_class,
+                    labels=self._labels,
+                )
 
                 name = f"{y_onehot.columns[i]} (AUC={auc_score:.2f})"
                 fig.add_trace(go.Scatter(x=fpr, y=tpr, name=name, mode="lines"))
@@ -377,6 +454,10 @@ class ROCCurves(Plan):
             return self._artifacts
 
     def display(self):
+        """
+        Display the plan's artifact. This method will be called by the IPython kernel to showcase the plan. If artifacts
+        were produced this is the method to draw and print them.
+        """
         if self._artifacts:
             display(HTML(self._artifacts[self._ARTIFACT_NAME].get_body()))
         else:
@@ -384,6 +465,8 @@ class ROCCurves(Plan):
 
 
 class FeatureImportance(Plan):
+    """ """
+
     _ARTIFACT_NAME = "feature_importance"
 
     def __init__(
@@ -391,44 +474,63 @@ class FeatureImportance(Plan):
         model=None,
         X_train=None,
     ):
+        """
+        :param model:
+        :param X_train:
+        """
         validate_numerical(X_train)
 
         super(FeatureImportance, self).__init__(model=model, X_train=X_train)
 
     def is_ready(self, stage: PlanStages) -> bool:
-        return stage == PlanStages.POST_FIT
+        """
+        Check whether or not the plan fits the given stage for production.
+        :param stage: The current stage in the function's process.
+        :return: True if the given stage fits the plan and False otherwise.
+        """
+        return stage == PlanStages.PRE_FIT
 
     def produce(self, model, X_train, **kwargs) -> Dict[str, PlotlyArtifact]:
         """
         Produce the artifact according to this plan.
         :return: The produced artifact.
         """
-        # Tree-based feature importance
-        if hasattr(model, "feature_importances_"):
-            importance_score = model.feature_importances_
+        if hasattr(model, "feature_importances_") or hasattr(model, "coef_"):
 
-        # Coefficient-based importance|
-        elif hasattr(model, "coef_"):
-            importance_score = model.coef_[0]
+            # Tree-based feature importance
+            if hasattr(model, "feature_importances_"):
+                importance_score = model.feature_importances_
 
-        df = pd.DataFrame(
-            {
-                "features": X_train.columns,
-                "feature_importance": importance_score,
-            }
-        ).sort_values(by="feature_importance", ascending=False)
+            # Coefficient-based importance|
+            elif hasattr(model, "coef_"):
+                importance_score = model.coef_[0]
 
-        fig = go.Figure(
-            [go.Bar(x=df["feature_importance"], y=df["features"], orientation="h")]
-        )
+            df = pd.DataFrame(
+                {
+                    "features": X_train.columns,
+                    "feature_importance": importance_score,
+                }
+            ).sort_values(by="feature_importance", ascending=False)
 
-        # Creating an html rendering of the plot
-        self._artifacts[self._ARTIFACT_NAME] = PlotlyArtifact(
-            figure=fig, key=self._ARTIFACT_NAME
-        )
-        return self._artifacts
+            fig = go.Figure(
+                [go.Bar(x=df["feature_importance"], y=df["features"], orientation="h")]
+            )
+
+            # Creating an html rendering of the plot
+            self._artifacts[self._ARTIFACT_NAME] = PlotlyArtifact(
+                figure=fig, key=self._ARTIFACT_NAME
+            )
+            return self._artifacts
+        else:
+            raise mlrun.errors.MLRunInvalidArgumentError(
+                "This model cannot be used for FeatureImportance plotting."
+            )
 
     def display(self):
+        """
+        Display the plan's artifact. This method will be called by the IPython kernel to showcase the plan. If artifacts
+        were produced this is the method to draw and print them.
+        """
         if self._artifacts:
             display(HTML(self._artifacts[self._ARTIFACT_NAME].get_body()))
         else:
@@ -436,6 +538,10 @@ class FeatureImportance(Plan):
 
 
 class LearningCurves(Plan):
+    """
+    Determines cross-validated training and test scores for different training set sizes.
+    """
+
     _ARTIFACT_NAME = "learning_curves"
 
     def __init__(
@@ -444,30 +550,83 @@ class LearningCurves(Plan):
         X_train=None,
         y_train=None,
         cv=3,
+        groups=None,
+        train_sizes=np.array([0.1, 0.33, 0.55, 0.78, 1.0]),
+        scoring=None,
+        exploit_incremental_learning=False,
+        n_jobs=None,
+        pre_dispatch="all",
+        verbose=0,
+        shuffle=False,
+        random_state=None,
+        return_times=True,
+        fit_params=None,
     ):
-        self._cv = cv
+        """
+
+        :param model: a trained model.
+        :param X_train: your
+        :param y_train:
+        :param cv: Determines the cross-validation splitting strategy.
+        :param groups: Group labels for the samples used while splitting the dataset into train/test set.
+        :param train_sizes: Relative or absolute numbers of training examples that will be used to generate the learning curve.
+        :param scoring: A str (see model evaluation documentation) or a scorer callable object / function with signature scorer(estimator, X, y).
+        :param exploit_incremental_learning: If the estimator supports incremental learning, this will be used to speed up fitting for different training set sizes.
+        :param n_jobs: Number of jobs to run in parallel.
+        :param pre_dispatch: Number of predispatched jobs for parallel execution (default is all).
+        :param verbose: Controls the verbosity: the higher, the more messages.
+        :param shuffle: Whether to shuffle training data before taking prefixes of it based on``train_sizes``.
+        :param random_state: Used when shuffle is True. Pass an int for reproducible output across multiple function calls.
+        :param return_times: Value to assign to the score if an error occurs in estimator fitting.
+        :param fit_params: Parameters to pass to the fit method of the estimator.
+        """
         validate_numerical(X_train)
         validate_numerical(y_train)
+
+        # learning_curve() params
+        self._groups = groups
+        self._cv = cv
+        self._train_sizes = train_sizes
+        self._scoring = scoring
+        self._exploit_incremental_learning = exploit_incremental_learning
+        self._n_jobs = n_jobs
+        self._pre_dispatch = pre_dispatch
+        self._verbose = verbose
+        self._shuffle = shuffle
+        self._random_state = random_state
+        self._return_times = return_times
+        self._fit_params = fit_params
 
         super(LearningCurves, self).__init__(
             model=model, X_train=X_train, y_train=y_train
         )
 
     def is_ready(self, stage: PlanStages) -> bool:
+        """
+        Check whether or not the plan fits the given stage for production.
+        :param stage: The current stage in the function's process.
+        :return: True if the given stage fits the plan and False otherwise.
+        """
         return stage == PlanStages.POST_FIT
 
     def produce(self, model, X_train, y_train, **kwargs) -> Dict[str, PlotlyArtifact]:
-        """
-        Produce the artifact according to this plan.
-        :return: The produced artifact.
-        """
 
         train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(
             model,
             X_train,
             y_train.values.ravel(),
+            groups=self._groups,
             cv=self._cv,
-            return_times=True,
+            train_sizes=self._train_sizes,
+            scoring=self._scoring,
+            _exploit_incremental_learning=self._exploit_incremental_learning,
+            n_jobs=self._n_jobs,
+            pre_dispatch=self._pre_dispatch,
+            verbose=self._verbose,
+            shuffle=self._shuffle,
+            random_state=self._random_state,
+            return_times=self._return_times,
+            fit_params=self._fit_params,
         )
 
         fig = go.Figure(
@@ -512,7 +671,125 @@ class LearningCurves(Plan):
         return self._artifacts
 
     def display(self):
+        """
+        Display the plan's artifact. This method will be called by the IPython kernel to showcase the plan. If artifacts
+        were produced this is the method to draw and print them.
+        """
         if self._artifacts:
             display(HTML(self._artifacts[self._ARTIFACT_NAME].get_body()))
         else:
             super(LearningCurves, self).display()
+
+
+class CalibrationCurve(Plan):
+    """
+    Compute true and predicted probabilities for a calibration curve.
+    The method assumes the inputs come from a binary classifier, and discretize the [0, 1] interval into bins.
+    """
+
+    _ARTIFACT_NAME = "calibration_curve"
+
+    def __init__(
+        self,
+        model=None,
+        X_test=None,
+        y_test=None,
+        normalize=False,
+        n_bins=5,
+        strategy="uniform",
+    ):
+        """
+
+        :param model: a fitted model capable of using predict_proba
+        :param X_test:
+        :param y_test:
+        :param normalize:Whether y_prob needs to be normalized into the [0, 1] interval, i.e. is not a proper probability.
+        :param n_bins: Number of bins to discretize the [0, 1] interval.
+        :param strategy: Strategy used to define the widths of the bins.
+        """
+        validate_numerical(X_test)
+        validate_numerical(y_test)
+
+        # calibration_curve() parameters
+        self._normalize = normalize
+        self._n_bins = n_bins
+        self._strategy = strategy
+
+        super(CalibrationCurve, self).__init__(
+            model=model, X_test=X_test, y_test=y_test
+        )
+
+    def is_ready(self, stage: PlanStages) -> bool:
+        """
+        Check whether or not the plan fits the given stage for production.
+        :param stage: The current stage in the function's process.
+        :return: True if the given stage fits the plan and False otherwise.
+        """
+        return stage == PlanStages.POST_FIT
+
+    def produce(self, model, X_test, y_test, **kwargs) -> Dict[str, PlotlyArtifact]:
+
+        if hasattr(model, "predict_proba"):
+            probs = model.predict_proba(X_test)[:, 1]
+            prob_true, prob_pred = calibration_curve(
+                y_test,
+                probs,
+                n_bins=self._n_bins,
+                normalize=self._normalize,
+                strategy=self._strategy,
+            )
+
+            fig = go.Figure(
+                data=[go.Scatter(x=prob_true, y=prob_pred)],
+                layout=dict(title=dict(text="Calibration Curve")),
+            )
+
+            # add custom xaxis title
+            fig.add_annotation(
+                dict(
+                    font=dict(color="black", size=14),
+                    x=0.5,
+                    y=-0.15,
+                    showarrow=False,
+                    text="prob_true",
+                    xref="paper",
+                    yref="paper",
+                )
+            )
+
+            # add custom yaxis title
+            fig.add_annotation(
+                dict(
+                    font=dict(color="black", size=14),
+                    x=-0.1,
+                    y=0.5,
+                    showarrow=False,
+                    text="prob_pred",
+                    textangle=-90,
+                    xref="paper",
+                    yref="paper",
+                )
+            )
+
+            # adjust margins to make room for yaxis title
+            fig.update_layout(margin=dict(t=100, l=100), width=800, height=500)
+
+            # Creating an html rendering of the plot
+            self._artifacts[self._ARTIFACT_NAME] = PlotlyArtifact(
+                figure=fig, key=self._ARTIFACT_NAME
+            )
+            return self._artifacts
+        else:
+            raise mlrun.errors.MLRunInvalidArgumentError(
+                "This model cannot be used for CalibrationCurve plotting."
+            )
+
+    def display(self):
+        """
+        Display the plan's artifact. This method will be called by the IPython kernel to showcase the plan. If artifacts
+        were produced this is the method to draw and print them.
+        """
+        if self._artifacts:
+            display(HTML(self._artifacts[self._ARTIFACT_NAME].get_body()))
+        else:
+            super(CalibrationCurve, self).display()
